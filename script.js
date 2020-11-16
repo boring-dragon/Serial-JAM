146bfb0f631c0a3b82f0b81e895d5834e10bf085
Mohamed jinas <j@live.mv>
2020-11-16 05:22:44 +0500

    delimeter customization

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const Readline = require('@serialport/parser-readline');
 
 serialPort = process.argv.slice(2)[0];
 baudRate = parseInt(process.argv.slice(3)[0]);
+delimiter = process.argv.slice(4)[0];
 
 const port = new SerialPort(serialPort, { baudRate: baudRate });
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
+const parser = port.pipe(new Readline({ delimiter: delimiter }));
 
 // Read the port data
 port.on("open", () => {
--- src/Serial.php
+++ src/Serial.php
@@ -9,10 +9,10 @@ class Serial
 {
     protected $port = "COM5";
     protected $baudRate = 9600;
+    protected $delimeter = "\n";
 
     public function setPort(string $port)
     {
-
         $this->port = $port;
     }
     public function setBaudRate(int $rate)
@@ -20,9 +20,14 @@ class Serial
         $this->baudRate = $rate;
     }
 
+    public function setDelimeter(string $delimeter)
+    {
+        $this->delimeter = $delimeter;
+    }
+
     public function execute()
     {
-        $process = new Process(["node", "script.js", $this->port, $this->baudRate]);
+        $process = new Process(["node", "script.js", $this->port, $this->baudRate, $this->delimeter]);
 
         $process->run(function ($type, $buffer) {
             if (Process::ERR === $type) {
