533ce8bf49fc4e101ed6f1e24998934ab0320a59
Mohamed jinas <boring-dragon@users.noreply.github.com>
2020-11-16 00:28:44 +0000

    Apply fixes from StyleCI

--- run.php
+++ run.php
@@ -4,8 +4,7 @@ require_once './vendor/autoload.php';
 
 use Jinas\Serial\Serial;
 
-
 $serial = new Serial();
-$serial->setPort("COM5")
+$serial->setPort('COM5')
        ->setBaudRate(115200)
        ->execute();
